4c9ebf830e003d44c4fe10b0bf22d85822569dfb
prajwalnarute <prajwalnarute100@gmail.com>
2025-07-02 23:23:44 +0530

    second commit

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,29 +7,35 @@ function Card(props) {
 
   return (
     <div className="cardContainer">
-      {
-        data.map((curItem, index) => {
-          // Using if-else to decide if the article should be rendered
-          if (curItem.urlToImage) {
-            // If there is an image URL, render the article
-            return (
-              <div className="card" key={index}>
-                <img src={curItem.urlToImage} alt="" />
-                <div className="cardContent">
-                  <a id="title" onClick={() => window.open(curItem.url)}>
-                    {curItem.title}
-                  </a>
-                  <p>{curItem.description}</p>
-                  <button onClick={() => window.open(curItem.url)}>Read More</button>
-                </div>
+      {data.map((curItem, index) => {
+        // Using if-else to decide if the article should be rendered
+        if (curItem.urlToImage) {
+          // If there is an image URL, render the article
+          return (
+            <div className="card" key={index}>
+              <img src={curItem.urlToImage} alt="" />
+              <div className="cardContent">
+                <a
+                  id="title"
+                  href={curItem.url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {curItem.title || "No title"}
+                </a>
+
+                <p>{curItem.description}</p>
+                <button onClick={() => window.open(curItem.url)}>
+                  Read More
+                </button>
               </div>
-            );
-          } else {
-            // If no image URL, don't render the article
-            return null;
-          }
-        })
-       }
+            </div>
+          );
+        } else {
+          // If no image URL, don't render the article
+          return null;
+        }
+      })}
     </div>
   );
 }
