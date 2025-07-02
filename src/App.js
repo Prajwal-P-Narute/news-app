28c8dad6e4d09ebe8a45faa48aa298d8416091e5
prajwalnarute <prajwalnarute100@gmail.com>
2025-07-02 23:10:55 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+  import './App.css';
+  import Newsapp from './components/Newsapp';
+
+
+  function App() {
+    return (
+      <div>
+        <Newsapp/>
+      </div>
+      
+    );
+  }
+
+  export default App;
